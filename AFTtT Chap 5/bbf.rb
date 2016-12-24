425202ba43362df5229bca570612e530cb7ea072
jdbrooks2020 <jdbrooks2020@berkeley.edu>
2016-12-24 11:19:56 -0800

    Files for Chapter 2 and 5

---
+++ AFTtT Chap 5/bbf.rb
@@ -0,0 +1,4 @@
+puts "What is your favorite number?"
+n_int = gets.to_i
+var = n_int +1
+puts "Isn\'t " + var.to_s + " bigger and better?"
---
+++ gets.rb
@@ -0,0 +1,2 @@
+puts "Is there an echo in here?"
+gets "Is there an echo in here?"
